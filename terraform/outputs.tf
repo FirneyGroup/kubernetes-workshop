4beddd4d3f96aa93f1bcd42d17abd1113e4dde60
Ashley Maloney <ashley.maloney@firney.com>
2023-07-18 21:34:33 +0100

    added outputs after terraform apply

---
+++ terraform/outputs.tf
@@ -0,0 +1,27 @@
+output "kubectl_connect" {
+  value = module.gke.kubectl_connect
+}
+
+output "redis_host" {
+  value = module.redis.redis_host
+}
+
+output "redis_port" {
+  value = module.redis.redis_port
+}
+
+output "sql_connection_name" {
+  value = module.cloudsql.sql_connection_name
+}
+
+output "sql_ip" {
+  value = module.cloudsql.sql_ip
+}
+
+output "sql_user" {
+  value = module.cloudsql.sql_user
+}
+
+output "sql_pass" {
+  value = module.cloudsql.sql_pass
+}
